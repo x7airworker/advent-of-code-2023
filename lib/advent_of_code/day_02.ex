37dfa59482ce88c89b4792ce9426bff97cfa02e8
x7airworker <jake@jakes-co.de>
2023-12-03 02:23:05 +0100

    Implemented day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,61 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  @max_red 12
+  @max_green 13
+  @max_blue 14
+
+  def part1(args) do
+    args
+      |> String.split("\n")
+      |> Enum.filter(& String.length(&1) > 0)
+      |> Enum.map(fn line ->
+        [raw_id, games] = String.split(line, ":")
+        parsed_id = raw_id
+          |> String.slice(5..-1)
+          |> Integer.parse()
+          |> elem(0)
+
+        impossible_count = String.split(games, ";")
+          |> Enum.map(&parse_game/1)
+          |> Enum.filter(fn x ->
+            Map.get(x, "red", 0) > @max_red or
+            Map.get(x, "green", 0) > @max_green or
+            Map.get(x, "blue", 0) > @max_blue
+          end)
+          |> Enum.count()
+
+        if impossible_count > 0, do: 0, else: parsed_id
+      end)
+     |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+      |> String.split("\n")
+      |> Enum.filter(& String.length(&1) > 0)
+      |> Enum.map(fn line ->
+        games = line
+          |> String.split(":")
+          |> List.last
+          |> String.split(";")
+          |> Enum.map(&parse_game/1)
+
+        Map.get(Enum.max_by(games, & Map.get(&1, "red", 0)), "red", 0) *
+        Map.get(Enum.max_by(games, & Map.get(&1, "green", 0)), "green", 0) *
+        Map.get(Enum.max_by(games, & Map.get(&1, "blue", 0)), "blue", 0)
+      end)
+     |> Enum.sum()
+  end
+
+  defp parse_game(game_str) do
+    game_str
+      |> String.split(",")
+      |> Enum.map(&String.trim/1)
+      |> Enum.reduce(%{}, fn x, acc ->
+        [value, key] = String.split(x, " ")
+        Map.put(acc, key, value
+          |> String.trim
+          |> Integer.parse
+          |> elem(0))
+      end)
   end
 end
